09321401d9d6d8a5ed345d0838d040fb47e8cecf
Akshay pal <akshaypal516@gmail.com>
2023-10-17 21:23:46 +0530

    changed footer

--- components/footer/footer1.vue
+++ components/footer/footer1.vue
@@ -1,172 +1,104 @@
 <template>
   <div>
-    <footer class="footer-light">
-      <div class="light-layout">
+    <footer class="bg-img-gym overlay-cls effect-cls footer-effect">
+      <div class="dark-layout">
         <div class="container">
-          <section class="small-section border-section border-top-0">
-            <div class="row">
-              <div class="col-lg-6">
-                <div class="subscribe">
-                  <div>
-                    <h4>KNOW IT ALL FIRST!</h4>
-                    <p>Never Miss Anything From Multikart By Signing Up To Our Newsletter.</p>
-                  </div>
+          <section class="section-b-space border-b">
+            <div class="row footer-theme2">
+              <div class="col-lg-3">
+                <div class="footer-title footer-mobile-title">
+                  <h4>about</h4>
+                </div>
+                <div class="footer-contant">
+                  <div class="footer-logo"></div>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et.Lorem ipsum dolor
+                    sit amet, consectetur adipiscing
+                  </p>
                 </div>
               </div>
-              <div class="col-lg-6">
-                <form
-                  class="form-inline subscribe-form auth-form needs-validation"
-                  method="post"
-                  id="mc-embedded-subscribe-form"
-                  name="mc-embedded-subscribe-form"
-                  target="_blank"
-                >
-                  <div class="form-group mx-sm-3">
-                    <input
-                      type="text"
-                      class="form-control"
-                      name="EMAIL"
-                      id="mce-EMAIL"
-                      placeholder="Enter your email"
-                      required="required"
-                    />
-                  </div>
-                  <button type="submit" class="btn btn-solid" id="mc-submit">subscribe</button>
-                </form>
+              <div class="col-lg-6 subscribe-wrapper">
+                <div class="subscribe-block">
+                  <h2>newsletter</h2>
+                  <form>
+                    <div class="form-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="exampleFormControlInput3"
+                        placeholder="Enter your email"
+                      />
+                      <button type="submit" class="btn btn-solid">
+                        subscribe
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="col-lg-3">
+                <div class="footer-title">
+                  <h4>store information</h4>
+                </div>
+                <div class="footer-contant">
+                  <ul class="contact-details">
+                    <li>Multikart Demo Store, Demo store India 345-659</li>
+                    <li>Call Us: 123-456-7898</li>
+                    <li>Email Us: <a href="#">Support@Multikart.com</a></li>
+                    <li>Fax: 123456</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </section>
         </div>
       </div>
-      <section class="section-b-space light-layout">
+      <div class="dark-layout">
         <div class="container">
-          <div class="row footer-theme partition-f">
-            <div class="col-lg-4 col-md-6">
-              <div class="footer-title footer-mobile-title">
-                <h4>about</h4>
-              </div>
-              <div class="footer-contant">
-                <div class="footer-logo">
-                  <img :src='"@/assets/images/icon/logo.png"' alt="logo" />
+          <section class="small-section">
+            <div class="row footer-theme2">
+              <div class="col p-set">
+                <div class="footer-link">
+                  <div class="footer-title">
+                    <h4>my account</h4>
+                  </div>
+                  <div class="footer-contant">
+                    <ul>
+                      <li><a href="#">mens</a></li>
+                      <li><a href="#">womens</a></li>
+                      <li><a href="#">clothing</a></li>
+                      <li><a href="#">accessories</a></li>
+                      <li><a href="#">featured</a></li>
+                    </ul>
+                  </div>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
-                <div class="footer-social">
-                  <ul>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-facebook" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-google-plus" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-instagram" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-rss" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                  </ul>
+                <div class="footer-link-b">
+                  <div class="footer-title">
+                    <h4>why we choose</h4>
+                  </div>
+                  <div class="footer-contant">
+                    <ul>
+                      <li><a href="#">shipping & return</a></li>
+                      <li><a href="#">secure shopping</a></li>
+                      <li><a href="#">gallary</a></li>
+                      <li><a href="#">affiliates</a></li>
+                      <li><a href="#">contacts</a></li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="col offset-xl-1">
-              <div class="sub-title">
-                <div class="footer-title">
-                  <h4>my account</h4>
-                </div>
-                <div class="footer-contant">
-                  <ul>
-                    <li>
-                      <a href="#">mens</a>
-                    </li>
-                    <li>
-                      <a href="#">womens</a>
-                    </li>
-                    <li>
-                      <a href="#">clothing</a>
-                    </li>
-                    <li>
-                      <a href="#">accessories</a>
-                    </li>
-                    <li>
-                      <a href="#">featured</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="sub-title">
-                <div class="footer-title">
-                  <h4>why we choose</h4>
-                </div>
-                <div class="footer-contant">
-                  <ul>
-                    <li>
-                      <a href="#">shipping & return</a>
-                    </li>
-                    <li>
-                      <a href="#">secure shopping</a>
-                    </li>
-                    <li>
-                      <a href="#">gallary</a>
-                    </li>
-                    <li>
-                      <a href="#">affiliates</a>
-                    </li>
-                    <li>
-                      <a href="#">contacts</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="sub-title">
-                <div class="footer-title">
-                  <h4>store information</h4>
-                </div>
-                <div class="footer-contant">
-                  <ul class="contact-list">
-                    <li>
-                      <i class="fa fa-map-marker"></i>Multikart Demo Store, Demo store India 345-659
-                    </li>
-                    <li>
-                      <i class="fa fa-phone"></i>Call Us: 123-456-7898
-                    </li>
-                    <li>
-                      <i class="fa fa-envelope-o"></i>Email Us:
-                      <a href="#">Support@Fiot.com</a>
-                    </li>
-                    <li>
-                      <i class="fa fa-fax"></i>Fax: 123456
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+          </section>
         </div>
-      </section>
-      <div class="sub-footer">
+      </div>
+      <div class="sub-footer darker-subfooter">
         <div class="container">
           <div class="row">
             <div class="col-xl-6 col-md-6 col-sm-12">
               <div class="footer-end">
                 <p>
-                  <i class="fa fa-copyright" aria-hidden="true"></i> 2017-18 themeforest powered by pixelstrap
+                  <i class="fa fa-copyright" aria-hidden="true"></i> 2023-24
+                  themeforest powered by pixelstrap
                 </p>
               </div>
             </div>
@@ -174,29 +106,19 @@
               <div class="payment-card-bottom">
                 <ul>
                   <li>
-                    <a href="#">
-                      <img :src='"@/assets/images/icon/visa.png"' alt />
-                    </a>
+                    <a href="#"> </a>
                   </li>
                   <li>
-                    <a href="#">
-                      <img :src='"@/assets/images/icon/mastercard.png"' alt />
-                    </a>
+                    <a href="#"> </a>
                   </li>
                   <li>
-                    <a href="#">
-                      <img :src='"@/assets/images/icon/paypal.png"' alt />
-                    </a>
+                    <a href="#"> </a>
                   </li>
                   <li>
-                    <a href="#">
-                      <img :src='"@/assets/images/icon/american-express.png"' alt />
-                    </a>
+                    <a href="#"> </a>
                   </li>
                   <li>
-                    <a href="#">
-                      <img :src='"@/assets/images/icon/discover.png"' alt />
-                    </a>
+                    <a href="#"> </a>
                   </li>
                 </ul>
               </div>
@@ -209,6 +131,5 @@
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
